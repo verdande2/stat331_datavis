1aaa60e8898b2b75932ab84b60de36174fd81744
Andrew Sparkes <andrew.sparkes@labs.cset.oit.edu>
2025-11-07 09:06:43 -0800

    made plotly graph work with reactive filtered data from car dataset. victory!

--- start_example/app.R
+++ start_example/app.R
@@ -8,98 +8,90 @@
 #
 
 library(shiny)
+library(plotly)
+library(bslib) # for tabset
+library(gt)
 
-# Define UI for application that draws a histogram
-# ui <- fluidPage(
-#     # Application title
-#     titlePanel("Old Faithful Geyser Data"),
-# 
-#     # Sidebar with a slider input for number of bins
-#     sidebarLayout(
-#         sidebarPanel(
-#             sliderInput("bins",
-#                         "Number of bins:",
-#                         min = 1,
-#                         max = 50,
-#                         value = 30)
-#         ),
-# 
-#         # Show a plot of the generated distribution
-#         mainPanel(
-#            plotOutput("distPlot")
-#         )
-#     )
-# )
+# clear out cwd
+rm(list = ls())
+
+data <- cars
+
 ui <- fluidPage(
   titlePanel(title = "Cars Dataset Dashboard"),
   sidebarLayout(
     sidebarPanel(
       sliderInput(
-        inputId = "slider_1",
-        label = "label",
-        min = 0,
-        max = 100,
-        value = 50
+        inputId = "slider_speed",
+        label = "Filter by Speed",
+        min = min(data$speed) - 2,
+        max = max(data$speed) + 2,
+        value = c(10, 20) # using a range in a c() makes the slider select an interval
       ),
       checkboxInput(
         inputId = "checkbox_regression",
-        label = "Regression"
+        label = "Fit Regression Line?"
       ),
       textOutput(outputId = "text_correlation_output")
     ),
-    mainPanel(verticalLayout(
-      fluidRow(plotlyOutput(outputId = "scatter_1")),
-      fluidRow(
-        column(
-          width = 6,
-          plotlyOutput(
-            outputId = "histogram_1",
-            height = "150px"
+    mainPanel(
+      navset_tab(
+        nav_panel("Car Dataset Vis Dashboard",
+          fluidRow(plotlyOutput(outputId = "scatter_1")),
+          fluidRow(
+            column(
+              width = 6,
+              plotlyOutput(
+                outputId = "histogram_1",
+                height = "150px"
+              )
+            ),
+            column(
+              width = 6,
+              plotlyOutput(
+                outputId = "histogram_2",
+                height = "150px"
+              )
+            )
           )
         ),
-        column(
-          width = 6,
-          plotlyOutput(
-            outputId = "histogram_2",
-            height = "150px"
+        nav_panel(
+          "Second Tab",
+          fluidRow(
+            dataTableOutput("filtered_data_table")
           )
         )
       )
-    ))
+    )
   )
 )
 
-# Define server logic required to draw a histogram
-# server <- function(input, output) {
-# 
-#     output$distPlot <- renderPlot({
-#         # generate bins based on input$bins from ui.R
-#         x    <- faithful[, 2]
-#         bins <- seq(min(x), max(x), length.out = input$bins + 1)
-# 
-#         # draw the histogram with the specified number of bins
-#         hist(x, breaks = bins, col = 'darkgray', border = 'white',
-#              xlab = 'Waiting time to next eruption (in mins)',
-#              main = 'Histogram of waiting times')
-#     })
-# }
 
 server <- function(input, output, session) {
+  filtered_data <- reactive({
+    data |> 
+      filter(speed >= input$slider_speed[1] & speed <= input$slider_speed[2]) # filter data in interval
+  })
+  
   output$scatter_1 <- renderPlotly({
-    fig = plot_ly(
-      data = iris, x = ~Sepal.Length, y = ~Petal.Length,
-      type = "scatter", mode = "markers"
-    ) %>%
-      layout(
-        title = "Sepal Length vs. Petal Length",
-        xaxis = list(title = "Sepal Length"),
-        yaxis = list(title = "Petal Length")
-      )
+    fil <- filtered_data()
+    fig = fil |>
+      plot_ly(
+      x = fil$speed, # using local var fil (and fil$colname) seems better than filtered_data()$colname for x, y
+      y = fil$dist,
+      type = "scatter",
+      mode = "markers"
+    ) |>
+    layout(
+      title = "Car Distance vs Speed",
+      xaxis = list(title = "Speed"),
+      yaxis = list(title = "Distance")
+    )
     fig
   })
   
   output$histogram_1 <- renderPlotly({
-    fig = plot_ly(data = diamonds, x = ~carat, type = "histogram", bingroup = 0.1) %>%
+    fig = plot_ly(data = diamonds, x = ~carat, type = "histogram", bingroup = 0.1) |>
       layout(
         title = "Diamond weight",
         xaxis = list(title = "Diamond weight"),
@@ -109,7 +101,7 @@ server <- function(input, output, session) {
   })
   
   output$histogram_2 <- renderPlotly({
-    fig = plot_ly(data = diamonds, x = ~carat, type = "histogram", bingroup = 0.1) %>%
+    fig = plot_ly(data = diamonds, x = ~carat, type = "histogram", bingroup = 0.1) |>
       layout(
         title = "Diamond weight",
         xaxis = list(title = "Diamond weight"),
@@ -119,8 +111,13 @@ server <- function(input, output, session) {
   })
   
   output$text_correlation_output <- renderText({
-    "This is an example for a textOutput."
+    x <- cor(filtered_data()) # calc corr coeff for speed/dist
+    x <- round(x, 3) # truncate off the correlation coeff to 3 decimal places
+    paste("The correlation between speed and distance is ", x[2], ".", sep="") # slam together the string with data
   })
+  
+  # on second tab, for filtering debug
+  output$filtered_data_table <- renderDataTable(expr = filtered_data())
 }
 
 # Run the application 
