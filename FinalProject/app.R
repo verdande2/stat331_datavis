f89a536c9199af483a7991a8de9e5be99524c1c1
Andrew Sparkes <verdande2@gmail.com>
2025-12-09 22:52:26 -0800

    adapting boilerplate example to my situation

--- FinalProject/app.R
+++ FinalProject/app.R
@@ -22,6 +22,7 @@ library(ggplot2)
 library(dplyr)
 
 # Load dataset
+
 ui <- dashboardPage(
   dashboardHeader(title = "Dynamic Dashboard"),
 
@@ -29,65 +30,100 @@ ui <- dashboardPage(
     sidebarMenu(
       id = "sidebar",
       menuItem("Columns", tabName = "select_variable"),
+      fluidRow(
+        ## Settings and config ---------------
+        # selectInput(
+        #   inputId = "select_dataset",
+        #   label = "Select Dataset:",
+        #   choices = c(
+        #     "diamonds" = "diamonds",
+        #     "mtcars" = "mtcars",
+        #     "iris" = "iris"
+        #   ),
+        #   selected = "diamonds"
+        # ),
 
-      tabItem(
-        tabName = "Selections",
-        fluidRow(
-          # Dataset selector (placed above column selector)
-          selectInput(
-            inputId = "select_dataset",
-            label = "Select Dataset:",
-            choices = c(
-              "diamonds" = "diamonds",
-              "mtcars" = "mtcars",
-              "iris" = "iris"
-            ),
-            selected = "diamonds"
-          ),
-
-          # Column selector
-          selectInput(
-            inputId = "select_col",
-            label = "Select Column:",
-            choices = NULL,
-            selectize = TRUE
-          )
+        ## Year selector ---------------
+        selectInput(
+          inputId = "select_year",
+          label = "Year:",
+          choices = NULL
+        ),
+        ## Input selector ---------------
+        selectInput(
+          inputId = "select_input",
+          label = "Input:",
+          choices = NULL
+        ),
+        ## Output selector ---------------
+        selectInput(
+          inputId = "select_output",
+          label = "Output:",
+          choices = NULL
         )
       ),
-      fluidRow(
-        conditionalPanel(
-          condition = "output.column_type == 'categorical'",
-          checkboxGroupInput(
-            inputId = "filter_categorical",
-            label = "Select Levels:",
-            choices = NULL,
-            selected = NULL
-          )
-        ),
-        conditionalPanel(
-          condition = "output.column_type == 'numeric'",
-          sliderInput(
-            inputId = "filter_numeric",
-            label = "Filter Numeric Data",
-            min = 0,
-            max = 100,
-            value = c(10, 30)
-          )
-        )
-      )
+      # fluidRow(
+      #   conditionalPanel(
+      #     condition = "output.column_type == 'categorical'",
+      #     checkboxGroupInput(
+      #       inputId = "filter_categorical",
+      #       label = "Select Levels:",
+      #       choices = NULL,
+      #       selected = NULL
+      #     )
+      #   ),
+      #   conditionalPanel(
+      #     condition = "output.column_type == 'numeric'",
+      #     sliderInput(
+      #       inputId = "filter_numeric",
+      #       label = "Filter Numeric Data",
+      #       min = 0,
+      #       max = 100,
+      #       value = c(10, 30)
+      #     )
+      #   )
+      # )
     )
   ),
 
   dashboardBody(
     fluidRow(
-      conditionalPanel(
-        condition = "output.column_type == 'categorical'",
-        uiOutput("dynamic_categorical_graphs")
+      # conditionalPanel(
+      #   condition = "output.column_type == 'categorical'",
+      #   uiOutput("dynamic_categorical_graphs")
+      # ),
+      # conditionalPanel(
+      #   condition = "output.column_type == 'numeric'",
+      #   uiOutput("dynamic_numeric_graphs")
+      # )
+      fluidRow(
+        plotlyOutput(
+          outputId = "plotScatter",
+          width = "100%",
+          height = "400px"
+        )
       ),
-      conditionalPanel(
-        condition = "output.column_type == 'numeric'",
-        uiOutput("dynamic_numeric_graphs")
-      )
+
+      fluidRow(
+        plotlyOutput(
+          outputId = "scatterGDP",
+          width = "100%",
+          height = "400px"
+        ),
+        outputId = "scatterCorruption",
+        width = "100%",
+        height = "400px"
+      ),
+      fluidRow(
+        plotlyOutput(
+          outputId = "scatterLife",
+          width = "100%",
+          height = "400px"
+        ),
+        outputId = "scatterFreedom",
+        width = "100%",
+        height = "400px"
+      ),
     )
   )
 )
